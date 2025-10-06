007486fb60e04fd3fd25a8902c1e0005512441c1
Keshav Parvat <keshavparvat123@gmail.com>
2025-10-06 15:06:15 +0530

    add files

---
+++ src/main/java/com/employee/management/controllers/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.employee.management.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.employee.management.entities.Employee;
+import com.employee.management.repository.EmployeeRepository;
+
+@RestController
+@CrossOrigin (origins = "http://localhost:4200")
+public class EmployeeController {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployee(){
+		return employeeRepository.findAll();
+	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return  employeeRepository.save(employee);
+	}
+
+}
